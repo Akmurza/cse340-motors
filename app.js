f8eb4f0ee4879d3bc7b73fac28f84f4c2bd79991
Akmurza <akuklin@byupathway.edu>
2026-01-16 21:09:12 +0400

    commit for CSE Motors home page

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set up EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+const indexRoutes = require('./routes/index');
+app.use('/', indexRoutes);
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
